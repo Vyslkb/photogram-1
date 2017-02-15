538a18094252f035ef553ffd900420fdc4ab55b1
Qays Alsharif <kaiis.alcherif@gmail.com>
2017-02-15 18:55:36 +0100

    Creating our Devise 'User' model : configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts
+  root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,6 +56,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :posts
-  root 'posts#index'
 end
